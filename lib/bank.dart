ded0676f1d0bf355202f3a7cf652141c69d5111e
ahmedibrahem <ahmed@gmail.com>
2020-09-12 12:23:19 +0200

    quizzler app

---
+++ lib/bank.dart
@@ -0,0 +1,8 @@
+class bank {
+  String question;
+  bool ans;
+  bank ({String q,bool a}){
+    question = q;
+    ans = a;
+  }
+}
---
+++ lib/easy.dart
@@ -0,0 +1,32 @@
+ import 'bank.dart';
+  class Easy{
+   
+ List<bank> seq = [ 
+    bank(q: 'Some cats are actually allergic to humans', a:true),
+    bank(q:'You can lead a cow down stairs but not up stairs.', a:false),
+    bank(q:'Approximately one quarter of human bones are in the feet.',a: true),
+    bank(q:'A slug\'s blood is green.', a:true),
+    bank(q:'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',a: true),
+    bank(q:'It is illegal to pee in the Ocean in Portugal.',a: true),
+    bank(
+        q:'No piece of square dry paper can be folded in half more than 7 times.',
+       a: false),
+    bank(
+        q:'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+       a: true),
+    bank(
+        q:'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        a:false),
+    bank(
+        q:'The total surface area of two human lungs is approximately 70 square metres.',
+        a:true),
+    bank(q:'Google was originally called \"Backrub\".', a: true),
+    bank(
+       q: 'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+       a: true),
+    bank(
+       q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        a:true),
+  ];
+ 
+ }
